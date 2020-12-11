a512f9ed229800ad6ceb029514c6113b8b8fb572
rachelf27 <rachelvfmurphy@gmail.com>
2020-12-10 16:36:35 -0800

    updated README, appspec and added more functionality to hello.cpp

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-This is a simple Hello World project created to be used to set up an AWS Pipeline
+This project is created to automate application deployment using AWS Pipeline as part of the AWS Certified DevOps Engineer Course.
+
+The local repository is configured to commit changes to both GitHub and AWS CodeCommit repositories. The AWS CodeDeploy uses revisions stored in GitHub to deploy to EC2/On-Premises.
+
+## hello.cpp
+This C++ app simply print out "Hello World" and current Date and Time on Linux OS.
+
+## buildspec.yml
+AWS CodeBuild agent uses buildspec.yml to run a build.
+
+## appspec.yml
+AWS CodeDeploy agent uses appspec.yml (application specification) to manage a deployment on EC2/On-Premise.
--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+using namespace std;
 
+#include <getopt.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+//function to read date and time from Linux OS
+void readOsDateTime(int *date, int *month,int *year, int *hour, int *min, int *sec, int *wday)
+{
+	time_t current = time(NULL);
+	struct tm *current_s = localtime(&current);
+
+	if(current_s != NULL)
+	{
+		*date  =  current_s	->tm_mday;
+		*month =  ++current_s	->tm_mon;
+		*year  =  current_s	->tm_year;
+
+		if(*year>100)
+			*year = *year - 100;
+
+		*hour  = current_s	->tm_hour;
+		*min   = current_s	->tm_min;
+		*sec   = current_s	->tm_sec;
+		*wday  = current_s	->tm_wday;
+	}
+}
+
+//main function
 int main()
 {
-  std::cout << "Hello, World!\n";
+	//init all variable to 0
+	int date   =0;
+	int month  =0;
+	int year   =0;
+	int hour   =0;
+	int min    =0;
+	int sec    =0;
+	int wday   =0;
+
+	//define weekdays names
+	char *Names[] = {"Sunday","Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"};
+
+  //print "Hello World"
+  cout << "Hello, World!\n";
+
+	//function call that will assign date and time in variables
+	readOsDateTime(&date, &month, &year, &hour, &min, &sec, &wday);
+	//printing the date
+  cout << "Current Date and Time:";
+	cout <<date<<"/"<<month<<"/"<<year+2000<<"  "<<hour<<":"<<min<<":"<<sec<<"  "<<Names[wday];
+	cout<<endl;    
+	return 0;
 }
